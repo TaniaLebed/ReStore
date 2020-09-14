e7456ea0a0c641ef401e97b278b207ff76e865a6
TaniaLebed <lebedtania666@gmail.com>
2020-09-14 10:49:29 +0300

    Updates

---
+++ precache-manifest.789cce0e11351a59de29aef3c01fa63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d4afcada1f2491e4a38b00cbfb4917",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "ff975afc8ce53f7f6126",
+    "url": "/re-store/static/css/main.abb251ac.chunk.css"
+  },
+  {
+    "revision": "e06da00227a6b3b70ce9",
+    "url": "/re-store/static/js/2.d37a1898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/re-store/static/js/2.d37a1898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff975afc8ce53f7f6126",
+    "url": "/re-store/static/js/main.c3b8fe17.chunk.js"
+  },
+  {
+    "revision": "1247206e4bacdd9497da",
+    "url": "/re-store/static/js/runtime-main.fc291269.js"
+  }
+]);
